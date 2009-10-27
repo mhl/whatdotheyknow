34fd5152d4a5911c79f1f4c37761adf21a80a9c8
Rowan Crawford <wombleton@gmail.com>
2009-10-28 11:53:57 +1300

    Making unicode friendly

--- config/environment.rb
+++ config/environment.rb
@@ -8,6 +8,8 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.4' unless defined? RAILS_GEM_VERSION
 
+$KCODE = 'u'
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091012103843) do
+ActiveRecord::Schema.define(:version => 20091027223448) do
 
   create_table "acts_as_xapian_jobs", :force => true do |t|
     t.string  "model",    :null => false
@@ -23,29 +23,29 @@ ActiveRecord::Schema.define(:version => 20091012103843) do
     t.integer  "info_request_id"
     t.integer  "user_id"
     t.integer  "public_body_id"
-    t.text     "text",              :null => false
-    t.text     "replacement",       :null => false
-    t.string   "last_edit_editor",  :null => false
-    t.text     "last_edit_comment", :null => false
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.text     "text",              :limit => 16777215, :null => false
+    t.text     "replacement",       :limit => 16777215, :null => false
+    t.string   "last_edit_editor",                      :null => false
+    t.text     "last_edit_comment", :limit => 16777215, :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
   end
 
   create_table "comments", :force => true do |t|
-    t.integer  "user_id",                                       :null => false
-    t.string   "comment_type",    :default => "internal_error", :null => false
+    t.integer  "user_id",                                                           :null => false
+    t.string   "comment_type",                        :default => "internal_error", :null => false
     t.integer  "info_request_id"
-    t.text     "body",                                          :null => false
-    t.boolean  "visible",         :default => true,             :null => false
-    t.datetime "created_at",                                    :null => false
-    t.datetime "updated_at",                                    :null => false
+    t.text     "body",            :limit => 16777215,                               :null => false
+    t.boolean  "visible",                             :default => true,             :null => false
+    t.datetime "created_at",                                                        :null => false
+    t.datetime "updated_at",                                                        :null => false
   end
 
   create_table "exim_log_dones", :force => true do |t|
-    t.text     "filename",   :null => false
-    t.datetime "last_stat",  :null => false
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.text     "filename",   :limit => 16777215, :null => false
+    t.datetime "last_stat",                      :null => false
+    t.datetime "created_at",                     :null => false
+    t.datetime "updated_at",                     :null => false
   end
 
   add_index "exim_log_dones", ["last_stat"], :name => "index_exim_log_dones_on_last_stat"
@@ -53,35 +53,35 @@ ActiveRecord::Schema.define(:version => 20091012103843) do
   create_table "exim_logs", :force => true do |t|
     t.integer  "exim_log_done_id"
     t.integer  "info_request_id"
-    t.integer  "order",            :null => false
-    t.text     "line",             :null => false
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.integer  "order",                                :null => false
+    t.text     "line",             :limit => 16777215, :null => false
+    t.datetime "created_at",                           :null => false
+    t.datetime "updated_at",                           :null => false
   end
 
   add_index "exim_logs", ["exim_log_done_id"], :name => "index_exim_logs_on_exim_log_done_id"
 
   create_table "holidays", :force => true do |t|
     t.date "day"
-    t.text "description"
+    t.text "description", :limit => 16777215
   end
 
   add_index "holidays", ["day"], :name => "index_holidays_on_day"
 
   create_table "incoming_messages", :force => true do |t|
-    t.integer  "info_request_id",        :null => false
-    t.datetime "created_at",             :null => false
-    t.datetime "updated_at",             :null => false
-    t.text     "cached_attachment_text"
-    t.text     "cached_main_body_text"
-    t.integer  "raw_email_id",           :null => false
+    t.integer  "info_request_id",                            :null => false
+    t.datetime "created_at",                                 :null => false
+    t.datetime "updated_at",                                 :null => false
+    t.text     "cached_attachment_text", :limit => 16777215
+    t.text     "cached_main_body_text",  :limit => 16777215
+    t.integer  "raw_email_id",                               :null => false
   end
 
   create_table "info_request_events", :force => true do |t|
-    t.integer  "info_request_id",     :null => false
-    t.text     "event_type",          :null => false
-    t.text     "params_yaml",         :null => false
-    t.datetime "created_at",          :null => false
+    t.integer  "info_request_id",                         :null => false
+    t.text     "event_type",          :limit => 16777215, :null => false
+    t.text     "params_yaml",         :limit => 16777215, :null => false
+    t.datetime "created_at",                              :null => false
     t.string   "described_state"
     t.string   "calculated_state"
     t.datetime "last_described_at"
@@ -94,46 +94,46 @@ ActiveRecord::Schema.define(:version => 20091012103843) do
   add_index "info_request_events", ["info_request_id"], :name => "index_info_request_events_on_info_request_id"
 
   create_table "info_requests", :force => true do |t|
-    t.text     "title",                                            :null => false
-    t.integer  "user_id",                                          :null => false
-    t.integer  "public_body_id",                                   :null => false
-    t.datetime "created_at",                                       :null => false
-    t.datetime "updated_at",                                       :null => false
-    t.string   "described_state",                                  :null => false
-    t.boolean  "awaiting_description",      :default => false,     :null => false
-    t.string   "prominence",                :default => "normal",  :null => false
-    t.text     "url_title",                                        :null => false
-    t.string   "law_used",                  :default => "foi",     :null => false
-    t.string   "allow_new_responses_from",  :default => "anybody", :null => false
-    t.string   "handle_rejected_responses", :default => "bounce",  :null => false
+    t.text     "title",                     :limit => 16777215,                        :null => false
+    t.integer  "user_id",                                                              :null => false
+    t.integer  "public_body_id",                                                       :null => false
+    t.datetime "created_at",                                                           :null => false
+    t.datetime "updated_at",                                                           :null => false
+    t.string   "described_state",                                                      :null => false
+    t.boolean  "awaiting_description",                          :default => false,     :null => false
+    t.string   "prominence",                                    :default => "normal",  :null => false
+    t.text     "url_title",                 :limit => 16777215,                        :null => false
+    t.string   "law_used",                                      :default => "foi",     :null => false
+    t.string   "allow_new_responses_from",                      :default => "anybody", :null => false
+    t.string   "handle_rejected_responses",                     :default => "bounce",  :null => false
   end
 
   add_index "info_requests", ["created_at"], :name => "index_info_requests_on_created_at"
 
   create_table "outgoing_messages", :force => true do |t|
-    t.integer  "info_request_id",              :null => false
-    t.text     "body",                         :null => false
-    t.string   "status",                       :null => false
-    t.string   "message_type",                 :null => false
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
+    t.integer  "info_request_id",                                  :null => false
+    t.text     "body",                         :limit => 16777215, :null => false
+    t.string   "status",                                           :null => false
+    t.string   "message_type",                                     :null => false
+    t.datetime "created_at",                                       :null => false
+    t.datetime "updated_at",                                       :null => false
     t.datetime "last_sent_at"
     t.integer  "incoming_message_followup_id"
-    t.string   "what_doing",                   :null => false
+    t.string   "what_doing",                                       :null => false
   end
 
   add_index "outgoing_messages", ["what_doing"], :name => "index_outgoing_messages_on_what_doing"
 
   create_table "post_redirects", :force => true do |t|
-    t.text     "token",              :null => false
-    t.text     "uri",                :null => false
-    t.text     "post_params_yaml"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
-    t.text     "email_token",        :null => false
-    t.text     "reason_params_yaml"
+    t.text     "token",              :limit => 16777215, :null => false
+    t.text     "uri",                :limit => 16777215, :null => false
+    t.text     "post_params_yaml",   :limit => 16777215
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+    t.text     "email_token",        :limit => 16777215, :null => false
+    t.text     "reason_params_yaml", :limit => 16777215
     t.integer  "user_id"
-    t.text     "circumstance",       :null => false
+    t.text     "circumstance",       :limit => 16777215, :null => false
   end
 
   add_index "post_redirects", ["updated_at"], :name => "index_post_redirects_on_updated_at"
@@ -144,49 +144,49 @@ ActiveRecord::Schema.define(:version => 20091012103843) do
   end
 
   create_table "public_bodies", :force => true do |t|
-    t.text     "name",                               :null => false
-    t.text     "short_name",                         :null => false
-    t.text     "request_email",                      :null => false
-    t.integer  "version",                            :null => false
-    t.string   "last_edit_editor",                   :null => false
-    t.text     "last_edit_comment",                  :null => false
-    t.datetime "created_at",                         :null => false
-    t.datetime "updated_at",                         :null => false
-    t.text     "url_name",                           :null => false
-    t.text     "home_page",                          :null => false
-    t.text     "notes",                              :null => false
-    t.string   "first_letter",                       :null => false
-    t.text     "publication_scheme",                 :null => false
-    t.text     "charity_number",                     :null => false
-    t.integer  "info_requests_count", :default => 0
+    t.text     "name",                :limit => 16777215,                :null => false
+    t.text     "short_name",          :limit => 16777215,                :null => false
+    t.text     "request_email",       :limit => 16777215,                :null => false
+    t.integer  "version",                                                :null => false
+    t.string   "last_edit_editor",                                       :null => false
+    t.text     "last_edit_comment",   :limit => 16777215,                :null => false
+    t.datetime "created_at",                                             :null => false
+    t.datetime "updated_at",                                             :null => false
+    t.text     "url_name",            :limit => 16777215,                :null => false
+    t.text     "home_page",           :limit => 16777215,                :null => false
+    t.text     "notes",               :limit => 16777215,                :null => false
+    t.string   "first_letter",                                           :null => false
+    t.text     "publication_scheme",  :limit => 16777215,                :null => false
+    t.text     "charity_number",      :limit => 16777215,                :null => false
+    t.integer  "info_requests_count",                     :default => 0
   end
 
   add_index "public_bodies", ["first_letter"], :name => "index_public_bodies_on_first_letter"
 
   create_table "public_body_tags", :force => true do |t|
-    t.integer  "public_body_id", :null => false
-    t.text     "name",           :null => false
-    t.datetime "created_at",     :null => false
+    t.integer  "public_body_id",                     :null => false
+    t.text     "name",           :limit => 16777215, :null => false
+    t.datetime "created_at",                         :null => false
   end
 
   create_table "public_body_versions", :force => true do |t|
     t.integer  "public_body_id"
     t.integer  "version"
-    t.text     "name"
-    t.text     "short_name"
-    t.text     "request_email"
+    t.text     "name",               :limit => 16777215
+    t.text     "short_name",         :limit => 16777215
+    t.text     "request_email",      :limit => 16777215
     t.datetime "updated_at"
     t.string   "last_edit_editor"
-    t.text     "last_edit_comment"
-    t.text     "url_name"
-    t.text     "home_page"
-    t.text     "notes"
-    t.text     "publication_scheme", :null => false
-    t.text     "charity_number",     :null => false
+    t.text     "last_edit_comment",  :limit => 16777215
+    t.text     "url_name",           :limit => 16777215
+    t.text     "home_page",          :limit => 16777215
+    t.text     "notes",              :limit => 16777215
+    t.text     "publication_scheme", :limit => 16777215, :null => false
+    t.text     "charity_number",     :limit => 16777215, :null => false
   end
 
   create_table "raw_emails", :force => true do |t|
-    t.text "data", :null => false
+    t.text "data", :limit => 16777215, :null => false
   end
 
   create_table "taggings", :force => true do |t|
@@ -239,17 +239,17 @@ ActiveRecord::Schema.define(:version => 20091012103843) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                                                     :null => false
-    t.string   "name",                                                      :null => false
-    t.string   "hashed_password",                                           :null => false
-    t.string   "salt",                                                      :null => false
-    t.datetime "created_at",                                                :null => false
-    t.datetime "updated_at",                                                :null => false
-    t.boolean  "email_confirmed",        :default => false,                 :null => false
-    t.text     "url_name",                                                  :null => false
-    t.datetime "last_daily_track_email", :default => '2000-01-01 00:00:00'
-    t.string   "admin_level",            :default => "none",                :null => false
-    t.text     "ban_text",                                                  :null => false
+    t.string   "email",                                                                         :null => false
+    t.string   "name",                                                                          :null => false
+    t.string   "hashed_password",                                                               :null => false
+    t.string   "salt",                                                                          :null => false
+    t.datetime "created_at",                                                                    :null => false
+    t.datetime "updated_at",                                                                    :null => false
+    t.boolean  "email_confirmed",                            :default => false,                 :null => false
+    t.text     "url_name",               :limit => 16777215,                                    :null => false
+    t.datetime "last_daily_track_email",                     :default => '2000-01-01 00:00:00'
+    t.string   "admin_level",                                :default => "none",                :null => false
+    t.text     "ban_text",               :limit => 16777215,                                    :null => false
     t.integer  "profile_photo_id"
   end
 
